6c510fa695f45e809c41b11909db5fd3e0086761
ezeqq3084_ <simonghost3084@gmail.com>
2025-11-04 22:43:46 -0300

    PS4

--- 900goldhen_psfree/lapse.mjs
+++ 900goldhen_psfree/lapse.mjs
@@ -1869,6 +1869,6 @@ setTimeout(PayloadLoader("aio_patches.bin"),500);
 log("AIO Fixes Applied.!");
 //Load GoldHEN :)
 setTimeout(PayloadLoader("goldhen_2.4b18.6.bin"),500);
-msgs.innerHTML = "GoldHEN v2.4b18.6 Loaded ...";
+msgs.innerHTML = "GoldHEN v2.4b18.6 Injectado ...";
 
 })
